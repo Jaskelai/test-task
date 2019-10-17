676a384db381b50d8a7b1cb101cbe4b3e3e68693
KornilovMikhail <kornilovmisha1999@gmail.com>
2019-10-17 10:58:36 +0300

    fixes

--- app/src/main/java/com/github/jaskelai/testtask/domain/DevicesInteractor.kt
+++ app/src/main/java/com/github/jaskelai/testtask/domain/DevicesInteractor.kt
@@ -3,13 +3,8 @@ package com.github.jaskelai.testtask.domain
 import com.github.jaskelai.testtask.data.BleDevicesRepository
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
 
 class DevicesInteractor(private val bleDevicesRepository: BleDevicesRepository) {
-
-    fun observeBluetooth(): Observable<Boolean> {
-        return bleDevicesRepository.observeBluetooth()
-    }
 
     fun observeBleDevices(): Observable<BleDevice> {
         return bleDevicesRepository.observeBleDevices()
